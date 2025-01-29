396902820f8b0a598384a6fcdd1f760e8bc189b6
Swapnil Mohite <swapnilmohite9565@gmail.com>
2025-01-29 10:05:03 +0530

    Updated dbConnect file

--- Backend/Config/DbConnect.js
+++ Backend/Config/DbConnect.js
@@ -2,16 +2,16 @@ const mongoose=require("mongoose");
 const dotenv=require("dotenv");
 dotenv.config();
 const ConnectDB=async()=>{
-   try {
-    const conn=await mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
-   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-   }
+   mongoose
+   .connect(process.env.MONGO_URL, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+   })
+   .then(() => console.log("✅ MongoDB Connected Successfully"))
+   .catch((err) => {
+       console.error("❌ MongoDB Connection Error:", err);
+       process.exit(1); // Stop server if connection fails
+   });
 }
 
 module.exports=ConnectDB;
